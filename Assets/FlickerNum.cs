a93da3c3951fb5afe75023d0beda592a8c3e45a3
Liam Sorta <liamsorta@gmail.com>
2017-04-30 09:31:32 +0100

    dontdieplease

---
+++ Assets/FlickerNum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlickerNum : MonoBehaviour {
+    Text me;
+    [SerializeField]
+    float min, max;
+
+    [SerializeField]
+    float minHack, maxHack;
+
+    bool hack;
+	// Use this for initialization
+	void Start () {
+        me = GetComponent<Text>();
+        if (me.text.Contains("/"))
+            hack = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if((int)Random.Range(0f,20f) == 10) {
+            Change();
+        }
+	}
+
+    void Change() {
+        if(hack)
+            me.text = ((int)Random.Range(min, max)).ToString() + "/" + ((int)Random.Range(minHack, maxHack)).ToString();
+        else
+            me.text = ((int)Random.Range(min, max)).ToString();
+
+    }
+}
---
+++ Assets/QuadUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadUpdate : MonoBehaviour {
+
+    public string sortingLayer;
+    public int orderInLayer;
+
+    void Awake() {
+        SetSortingLayer();
+    }
+
+    [ContextMenu("Update sorting layer settings")]
+    void UpdateSortingLayerSettings() {
+        SetSortingLayer();
+    }
+
+    private void SetSortingLayer() {
+        Renderer rend = GetComponent<Renderer>();
+        rend.sortingLayerName = sortingLayer;
+        rend.sortingOrder = orderInLayer;
+    }
+}
