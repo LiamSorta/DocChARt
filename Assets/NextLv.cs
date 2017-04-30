5c50b0129fc8d0d4b6db1d6e11a31e25855d95a1
Liam Sorta <liamsorta@gmail.com>
2017-04-30 12:16:50 +0100

    woooooooo
    done

---
+++ Assets/NextLv.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextLv : MonoBehaviour {
+
+    public Text thing;
+    public void GoGo() {
+        if (thing.text == "pARssword") {
+            Application.LoadLevel(1);
+        } else {
+            thing.gameObject.transform.parent.gameObject.GetComponent<InputField>().interactable = false;
+        }
+    }
+}
